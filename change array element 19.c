d5e4515044c25138e01981c0c77fbdc452b4adc8
Debela394 <debela394@gmail.com>
2024-04-13 12:29:48 +0300

    Array element changed

--- change array element 19.c
+++ change array element 19.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    
-    int age[5] = {25,23,21,26,28};
-    
-        age[2] = 30;
-    
+int main()
+{
+
+    int age[5] = {25, 23, 21, 26, 28};
+
+    age[1] = 22;
+
     printf("The new 3rd array element is: %d", age[2]);
-    
+
     return 0;
 }
